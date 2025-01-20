2826a820d926a3599217339700b4458acea9811c
Marcelo Oliveira <marcelojos2010@hotmail.com>
2025-01-20 17:44:15 -0300

    Construção do jogo Mario Jump (incompleto)

--- POO/20250114/05/InterfaceIdioma.java
+++
@@ -1,3 +0,0 @@
-public interface InterfaceIdioma {
-    String idioma();
-}
---
+++ POO/20250114/aula_python/ex.py
@@ -0,0 +1,4 @@
+#F=(9*C+160) formula de conversão
+tempC = float(input('Digite a temperatura em Celcius'))
+tempF = (9*tempC + 160) / 5
+print(' A temperatura em Celsius é de : {} C, \nA temperatura em Fahrenheit é {}F'.format(tempC, tempF))
